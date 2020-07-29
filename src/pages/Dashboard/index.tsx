2fa752bbe93b516389e8fb4f706dacb81ab7508e
BrunoSaibert <brunosaibert@gmail.com>
2020-07-29 18:58:05 -0300

    fix: page dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import Hero from '../../components/Hero';
 
-import * as S from './styles';
-
 const Dashboard: React.FC = () => {
   return <Hero />;
 };
--- src/pages/Dashboard/styles.ts
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const Title = styled.h1`
-  color: #7159c1;
-`;
