c19deb52de9894e1c7b0ced7346b247b3d1e7634
BrunoSaibert <brunosaibert@gmail.com>
2020-07-29 14:56:24 -0300

    feat: adiciona editorconfig, eslint e prettier

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, // aspas simples
+  trailingComma: "all", // virgula no final dos objetos
+  arrowParens: "avoid", // arrow function sem parentesis quando tiver apenas um parámetro
+};
