1fbe8f4e31296c2a75b504d604e5e5c0e7a2f176
BRUNO H SAIBERT <40339324+BrunoSaibert@users.noreply.github.com>
2020-08-21 18:48:57 -0300

    Update index.tsx

--- src/pages/MyList/index.tsx
+++ src/pages/MyList/index.tsx
@@ -24,7 +24,7 @@ const MyList: React.FC = () => {
 
         setIsLoading(false);
       });
-  }, [type]);
+  }, []);
 
   const getQtdCardsVisible = useCallback(() => {
     setQtdCards(getQtdDimensions());
