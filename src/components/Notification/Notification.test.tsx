9854be35cc56cb01c731d1cf5a13c8c3eb7562a0
Frontend <frontend_chapter@pbtech.net.br>
2020-10-22 14:07:57 -0300

    feat: add notification menu

--- src/components/Notification/Notification.test.tsx
+++ src/components/Notification/Notification.test.tsx
@@ -2,23 +2,42 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { axe } from 'jest-axe';
 import { Notification } from '.';
+import notifications from '../../assets/json/notifications.json';
 
-const wrapper = (count?: number) => render(<Notification count={count} />);
+interface NotificationMovieProps {
+  img: string;
+  title: string;
+  movie: string;
+  days: string;
+}
+interface NotificationProps {
+  count?: number;
+  movies?: Array<NotificationMovieProps>;
+}
+
+const wrapper = ({ count, movies }: NotificationProps) =>
+  render(<Notification count={count} movies={movies} />);
 
 it('Notification Component', async () => {
-  const { container } = wrapper(4);
+  const { container } = wrapper({ count: 4 });
 
   expect(await axe(container)).toHaveNoViolations();
 });
 
 it('Show badget if have notification', async () => {
-  wrapper(4);
+  wrapper({ count: 4 });
 
   expect(screen.getByText(/4/i)).toBeInTheDocument();
 });
 
 it("Do not Show badget if don't have notification", async () => {
-  wrapper();
+  wrapper({});
 
   expect(screen.queryByText('4')).toBeNull();
+});
+
+it('Render notification movies', async () => {
+  wrapper({ count: 3, movies: notifications });
+
+  expect(screen.queryByText('3')).toBeInTheDocument();
 });
--- src/components/Notification/styles.ts
+++ src/components/Notification/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+  position: relative;
+`;
+
 export const Button = styled.button`
   position: relative;
   background: transparent;
@@ -14,4 +18,57 @@ export const Badge = styled.span`
   border-radius: 50%;
   background-color: red;
   color: white;
+`;
+
+export const Modal = styled.ul`
+  display: flex;
+  flex-direction: column;
+
+  position: absolute;
+  top: 25px;
+  right: 0;
+  width: 400px;
+
+  background: rgba(0, 0, 0, 0.5);
+  border: 1px solid gray;
+`;
+
+export const Item = styled.li`
+  display: flex;
+  border-bottom: 1px solid gray;
+
+  :last-child {
+    border: none;
+  }
+`;
+
+export const Link = styled.a`
+  display: flex;
+`;
+
+export const Thumbnail = styled.img`
+  width: 120px;
+  height: 60px;
+  margin: 10px;
+`;
+
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+`;
+
+export const Title = styled.p`
+  font-size: 16px;
+  color: #ccc;
+  font-weight: bold;
+  text-align: left;
+`;
+
+export const Days = styled.p`
+  font-size: 10px;
+  color: #999;
+  font-weight: bold;
+  padding-top: 5px;
 `;
